b836f55fb999772d410d1d030a7a60621346382e
Hitesh <hitesh.kumar2309@gmail.com>
2022-07-30 02:36:41 +0530

    Creating blog posts exercise-5

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('blogpost/', views.blogpost, name='blogpost' )
+    path('blogpost/', views.blogpost, name='blogpost')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Blogpost   
 
 # Create your views here.
 def index(request):
-    return render(request, 'blog/index.html')
+    allPosts = []
+    posts = Blogpost.objects.all()
+    catprods = Blogpost.objects.values('title', 'head0')
+    cats = {item['title'] for item in catprods}
+    # print("cats = ", cats)
+    for items in range(0, len(cats)):
+        # print('items = ', items)
+        title = posts[items]
+        time = posts[items].pub_date
+        chead0 = posts[items].head_content0
+        thumbnail = posts[items].thumbnail
+        allPosts.append([title, time, chead0, thumbnail])
+        
+    params = { 'allPosts' : allPosts }
+    # print("params = ", params)
+    return render(request, 'blog/index.html', params)
 
 def blogpost(request):
     return render(request, 'blog/blogpost.html')
