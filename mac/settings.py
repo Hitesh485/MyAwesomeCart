e1f0e61dc12af0ed341b2caabf871f3c91317d8a
Hitesh485 <hitesh.kumar2309@gmail.com>
2022-09-01 22:04:29 +0530

    create .env file and hide secret key

--- mac/settings.py
+++ mac/settings.py
@@ -1,4 +1,3 @@
-import os
 """
 Django settings for mac project.
 
@@ -12,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!$e#+(-u19p359176*16s_f(k*yp(1-iotpch1p(d3$0uru!^y'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
